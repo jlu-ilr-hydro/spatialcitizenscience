d6fe3c5e60d648aeef0a27e70f984b34fb30e133
philippkraft <philipp.kraft@umwelt.uni-giessen.de>
2018-09-13 11:54:29 +0200

    Started a database template (not working, just a stub)

--- database.py
+++ database.py
@@ -1,7 +1,17 @@
 from sqlite3 import connect
 from configuration import get_config
+
+
 def write_entry(lon: float, lat: float, length: float, width: float, depth:float, comment:str) -> int:
     config = get_config()
+    dbconf = config.database
+    values = lon, lat, length, width, depth, comment
+    with connect(dbconf.filename) as con:
+        con.execute(f'INSERT INTO {tn} ({fields}) VALUES ({values})'.format(
+            tn=dbconf.tablename,
+            fields=', '.join(dbconf.fields),
+            values=','.join('?' * len(dbconf.fields))
+        ),
+            values
 
-    with connect(config.database) as con:
-        con.execute(f'INSERT INTO {tn} (lon, lat, )'.format(...))
+        )
