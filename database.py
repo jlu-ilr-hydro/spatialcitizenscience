24a433ca67a687dcfe708d76f03740a491492982
philippkraft <philipp.kraft@umwelt.uni-giessen.de>
2018-09-14 14:38:55 +0200

    Complete configurable database

--- database.py
+++ database.py
@@ -1,17 +1,106 @@
-from sqlite3 import connect
-from .configuration import get_config
+import sqlite3
+from contextlib import contextmanager
+
+from configuration import get_config
+debug = False
+
+python_to_sql_type = dict(
+    str='TEXT',
+    int='INTEGER',
+    float='REAL',
+    bytes='BLOB',
+    datetime='TEXT'
+)
 
 
-def write_entry(lon: float, lat: float, length: float, width: float, depth:float, comment:str) -> int:
-    config = get_config()
-    dbconf = config.database
-    values = lon, lat, length, width, depth, comment
-    with connect(dbconf.filename) as con:
-        con.execute(f'INSERT INTO {tn} ({fields}) VALUES ({values})'.format(
-            tn=dbconf.tablename,
-            fields=', '.join(dbconf.fields),
-            values=','.join('?' * len(dbconf.fields))
-        ),
-            values
+class Connection:
 
+    def __init__(self, config=None):
+        config = config or get_config().database
+        self.tablename = config.tablename
+        self.__connection = sqlite3.connect(config.filename)
+        self.fields = config.fields
+        self.fieldnames = [f.name for f in self.fields]
+
+    def create(self):
+        """
+        Creates the tables for the database
+        :param config: The database configuration (get_config().database)
+        :return:
+        """
+        columns = ',\n'.join(
+            ['    {name} {type}'
+                .format(name=col.name, type=python_to_sql_type[col.type])
+             for col in self.fields])
+
+        cmd = ('CREATE TABLE IF NOT EXISTS {tablename} (' +
+               '\n    id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+               '\n{columns}\n);').format(
+            tablename=self.tablename,
+            columns=columns)
+
+        if debug:
+            print(cmd)
+
+        c = self.execute(cmd)
+        if debug:
+            print('    ->', c.fetchall())
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__connection.close()
+
+    def execute(self, cmd, args=()):
+        return self.__connection.execute(cmd, args)
+
+    def write_entry(self, **kwargs):
+        """
+        Writes an entry into the database
+        :param kwargs:
+        :return:
+        """
+        values = [kwargs.get(f) for f in self.fieldnames]
+        cmd = 'INSERT INTO {tn} ({fields}) VALUES ({values})'.format(
+            tn=self.tablename,
+            fields=', '.join([f.name for f in self.fields]),
+            values=','.join('?' * len(self.fields))
         )
+        if debug:
+            print(cmd)
+
+        c = self.execute(cmd, values)
+        if debug:
+            print('    ->', c.fetchall())
+
+    def commit(self):
+        self.__connection.commit()
+
+    def count(self):
+        cmd = 'SELECT count(*) FROM ' + self.tablename
+        if debug:
+            print(cmd)
+
+        c = self.execute(cmd)
+        r = c.fetchone()
+        if debug:
+            print('    ->', r)
+        return r[0]
+
+    def read_entries(self, **kwargs):
+        cmd = 'SELECT * FROM ' + self.tablename
+        if kwargs:
+            fields, values = zip(*kwargs.items())
+            cmd += '\nWHERE\n    '
+            cmd += ' AND \n    '.join(('{}=?'.format(k) for k in fields))
+        else:
+            values = ()
+        cmd += ';'
+
+        if debug:
+            print(cmd)
+
+        c = self.execute(cmd, values)
+        return c.fetchall()
+
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,8 @@
 import unittest
-
-
+import database as db
+import random
 from configuration import get_config, home
+
 
 class TestConfig(unittest.TestCase):
     def test_config_file(self):
@@ -13,3 +14,47 @@ class TestConfig(unittest.TestCase):
         db_conf = config.database
         self.assertEqual(db_conf.filename, 'entries.sqlite')
         self.assertEqual(db_conf.tablename, 'entries')
+
+
+def make_entry(config):
+    return {f.name: random.random() * 100
+            for f in config.fields
+            }
+
+
+class TestDb(unittest.TestCase):
+
+    def test_init_db(self):
+        config = get_config()
+        config.database.filename = ':memory:'
+        with db.Connection(config.database) as con:
+            con.create()
+
+    def test_write_db(self):
+        config = get_config()
+        config.database.filename = ':memory:'
+        with db.Connection(config.database) as con:
+            con.create()
+            entry = make_entry(config.database)
+            con.write_entry(**entry)
+            con.commit()
+            self.assertEqual(con.count(), 1)
+
+    def test_read_db(self):
+        config = get_config()
+        config.database.filename = ':memory:'
+        with db.Connection(config.database) as con:
+            con.create()
+            for i in range(100):
+                entry = make_entry(config.database)
+                con.write_entry(**entry)
+            con.commit()
+            self.assertEqual(con.count(), 100)
+            result = con.read_entries()
+            print(result[-1])
+            self.assertEqual(len(result), 100)
+            self.assertCountEqual([r[0] for r in result], range(1, 101))
+            result = con.read_entries(id=10)
+            self.assertEqual(len(result), 1, 'Filtered for id=10 but got too many results (or None)')
+
+
