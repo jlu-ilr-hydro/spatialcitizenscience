186c05bf47353825caeedccbdf93d6666c97203d
Philipp Kraft <philipp.kraft@envr.jlug.de>
2022-02-04 12:34:05 +0100

    Fixed #2 and some minor other bugs

--- spatialcitizenscience/__init__.py
+++ spatialcitizenscience/__init__.py
@@ -17,9 +17,9 @@ def create_app(test_config=None):
     with Config() as config:
         app.config.from_mapping(config)
         app.add_template_global(config, 'config')
+        app.register_blueprint(view.ui, url_prefix=config.base_url)
     if test_config:
         app.config.from_mapping(test_config)
-    app.register_blueprint(view.ui)
     app.add_template_filter(view.clean)
     db.debug = app.config['DEBUG']
     return app
